cbef9717bff032a79258d1e30f71e75f5f5864fc
ludovico <ludovicopestarino00@gmail.com>
2020-04-04 01:36:19 +0200

    First commit

--- parser.py
+++ parser.py
@@ -4,8 +4,6 @@ import random
 with open('citData.json') as json_data:
 	jsonData = json.load(json_data)
 
-print (random.choice(jsonData))
-
-#for i in jsonData:
-#	print (i['Author'])
-#	print (i['text'])
+for i in jsonData:
+	print (i['Author'])
+	print (i['text'])
